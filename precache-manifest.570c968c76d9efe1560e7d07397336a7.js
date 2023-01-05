0a546fd51650f6021678a63e2668488b9ffc2ed4
Ishaan-Morgan <121957622+Ishaan-Morgan@users.noreply.github.com>
2023-01-05 17:38:03 +0200

    create GID

---
+++ precache-manifest.570c968c76d9efe1560e7d07397336a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a44f2c82776c37dbfa3f63afcd1360fc",
+    "url": "/Game_in_Dots/index.html"
+  },
+  {
+    "revision": "48d730b7e055766a055a",
+    "url": "/Game_in_Dots/static/css/2.96121624.chunk.css"
+  },
+  {
+    "revision": "59af25ea47bfe8df2301",
+    "url": "/Game_in_Dots/static/css/main.059a3562.chunk.css"
+  },
+  {
+    "revision": "48d730b7e055766a055a",
+    "url": "/Game_in_Dots/static/js/2.a4f8db00.chunk.js"
+  },
+  {
+    "revision": "59af25ea47bfe8df2301",
+    "url": "/Game_in_Dots/static/js/main.55447a24.chunk.js"
+  },
+  {
+    "revision": "efef8b1d4dc0b1eb182b",
+    "url": "/Game_in_Dots/static/js/runtime-main.d02e9600.js"
+  }
+]);
